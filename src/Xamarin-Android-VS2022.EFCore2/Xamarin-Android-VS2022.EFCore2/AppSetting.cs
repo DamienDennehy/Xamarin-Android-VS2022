c36f2bb255fd2f6f415b4b3d4a13deecf0f015ea
Damien Dennehy <DamienDennehy@users.noreply.github.com>
2022-12-22 15:23:53 +0000

    Added simple EF Core 2 query/

--- src/Xamarin-Android-VS2022.EFCore2/Xamarin-Android-VS2022.EFCore2/AppSetting.cs
+++ src/Xamarin-Android-VS2022.EFCore2/Xamarin-Android-VS2022.EFCore2/AppSetting.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Xamarin_Android_VS2022.EFCore2
 {
     [Table(nameof(AppSetting))]
     public class AppSetting
     {
+        [Key]
         public string Key { get; set; }
         public string Value { get; set; }
     }
