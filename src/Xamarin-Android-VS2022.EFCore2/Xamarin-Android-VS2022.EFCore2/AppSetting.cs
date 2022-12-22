b7e2624b36d167fbf6bca1fb54e1191ed9ab902c
Damien Dennehy <DamienDennehy@users.noreply.github.com>
2022-12-22 14:50:17 +0000

    Added EFCore 2 project.

---
+++ src/Xamarin-Android-VS2022.EFCore2/Xamarin-Android-VS2022.EFCore2/AppSetting.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Xamarin_Android_VS2022.EFCore2
+{
+    [Table(nameof(AppSetting))]
+    public class AppSetting
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
